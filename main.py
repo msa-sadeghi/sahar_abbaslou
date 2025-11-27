820c94a5bc4bf22440d4c846cbcffa63c88beea3
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-27 17:41:47 +0330

    codes

--- main.py
+++ main.py
@@ -21,10 +21,18 @@ myfont = pygame.font.Font("f.ttf", 28)
 meat_counts = 0
 meat_text = myfont.render(f"meat {meat_counts}", True, "green")
 lives = 5
-lives_text = myfont.render(f"lives {lives}", True, "red")
+lives_text = myfont.render(f"lives {lives}", True, "black")
+
+game_over_text = myfont.render("Game Over Please press Space to play  again", True, "black")
+game_over_rect = game_over_text.get_rect(center=(WIDTH/2, HEIGHT/2))
 
 FPS = 60
 CLOCK = pygame.time.Clock()
+playing_mode = True
+
+hit_sound =  pygame.mixer.Sound("hit.wav")
+
+
 running = True
 while running:
     CLOCK.tick(FPS)
@@ -32,41 +40,56 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        tiger_rect.x -= 5
-    if keys[pygame.K_RIGHT]:
-        tiger_rect.x += 5  
 
-    meat_rect.y += 5  
-    bomb_rect.y += 5  
-    if tiger_rect.colliderect(meat_rect):
-        random_x = random.randint(80, 920)
-        meat_rect.x = random_x
-        meat_rect.y = 0
-        meat_counts += 1
-        
-    if meat_rect.bottom >= 640:
-        random_x = random.randint(80, 920)
-        meat_rect.x = random_x
-        meat_rect.y = 0
+    if lives <= 0:
+        playing_mode = False
+    if playing_mode:
 
-    if tiger_rect.colliderect(bomb_rect):
-        random_x = random.randint(80, 920)
-        bomb_rect.x = random_x
-        bomb_rect.y = 0
-        lives -= 1
+        if keys[pygame.K_LEFT]:
+            tiger_rect.x -= 5
+        if keys[pygame.K_RIGHT]:
+            tiger_rect.x += 5  
 
-    if bomb_rect.bottom >= 640:
-        random_x = random.randint(80, 920)
-        bomb_rect.x = random_x
-        bomb_rect.y = 0
+        meat_rect.y += 5  
+        bomb_rect.y += 5  
+        if tiger_rect.colliderect(meat_rect):
+            random_x = random.randint(80, 920)
+            meat_rect.x = random_x
+            meat_rect.y = 0
+            meat_counts += 1
+            hit_sound.play()
+
+        if meat_rect.bottom >= 640:
+            random_x = random.randint(80, 920)
+            meat_rect.x = random_x
+            meat_rect.y = 0
+
+        if tiger_rect.colliderect(bomb_rect):
+            random_x = random.randint(80, 920)
+            bomb_rect.x = random_x
+            bomb_rect.y = 0
+            lives -= 1
+
+        if bomb_rect.bottom >= 640:
+            random_x = random.randint(80, 920)
+            bomb_rect.x = random_x
+            bomb_rect.y = 0
 
     meat_text = myfont.render(f"meat {meat_counts}", True, "green")
-    lives_text = myfont.render(f"lives {lives}", True, "red")
-    screen.fill("pink")
-    screen.blit(tiger_image, tiger_rect)
-    screen.blit(meat_image, meat_rect)
-    screen.blit(bomb_image, bomb_rect)
+    lives_text = myfont.render(f"lives {lives}", True, "black")
+    if playing_mode:
+        screen.fill("pink")
+    else:
+        screen.fill("red")
+        screen.blit(game_over_text, game_over_rect)
+        if keys[pygame.K_SPACE]:
+            playing_mode = True
+            lives  = 5
+            meat_counts = 0
+    if playing_mode:
+        screen.blit(tiger_image, tiger_rect)
+        screen.blit(meat_image, meat_rect)
+        screen.blit(bomb_image, bomb_rect)
     screen.blit(meat_text, (10, 10))
     screen.blit(lives_text, (10, 50))
     pygame.display.update()
