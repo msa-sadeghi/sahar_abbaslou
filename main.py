fc60841826b654c15eaed126293e3d61928b825e
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-27 17:27:04 +0330

    codes

--- main.py
+++ main.py
@@ -13,9 +13,15 @@ meat_image = pygame.image.load("meat.png")
 meat_image = pygame.transform.scale_by(meat_image, 0.08)
 meat_rect = meat_image.get_rect(midtop=(500, 0))
 
+bomb_image = pygame.image.load("bomb.png")
+bomb_image = pygame.transform.scale_by(bomb_image, 0.8)
+bomb_rect = bomb_image.get_rect(midtop=(500, 0))
+
 myfont = pygame.font.Font("f.ttf", 28)
 meat_counts = 0
 meat_text = myfont.render(f"meat {meat_counts}", True, "green")
+lives = 5
+lives_text = myfont.render(f"lives {lives}", True, "red")
 
 FPS = 60
 CLOCK = pygame.time.Clock()
@@ -32,15 +38,35 @@ while running:
         tiger_rect.x += 5  
 
     meat_rect.y += 5  
-    if tiger_rect.colliderect(meat_rect) or \
-    meat_rect.bottom >= 640:
+    bomb_rect.y += 5  
+    if tiger_rect.colliderect(meat_rect):
         random_x = random.randint(80, 920)
         meat_rect.x = random_x
         meat_rect.y = 0
         meat_counts += 1
+        
+    if meat_rect.bottom >= 640:
+        random_x = random.randint(80, 920)
+        meat_rect.x = random_x
+        meat_rect.y = 0
+
+    if tiger_rect.colliderect(bomb_rect):
+        random_x = random.randint(80, 920)
+        bomb_rect.x = random_x
+        bomb_rect.y = 0
+        lives -= 1
+
+    if bomb_rect.bottom >= 640:
+        random_x = random.randint(80, 920)
+        bomb_rect.x = random_x
+        bomb_rect.y = 0
+
     meat_text = myfont.render(f"meat {meat_counts}", True, "green")
+    lives_text = myfont.render(f"lives {lives}", True, "red")
     screen.fill("pink")
     screen.blit(tiger_image, tiger_rect)
     screen.blit(meat_image, meat_rect)
+    screen.blit(bomb_image, bomb_rect)
     screen.blit(meat_text, (10, 10))
+    screen.blit(lives_text, (10, 50))
     pygame.display.update()
